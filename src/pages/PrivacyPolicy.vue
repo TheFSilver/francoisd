1b38e8c5909d6f51f25df59df6bb7bfc248b56fa
Frank Silver <dasylva.f@gmail.com>
2023-05-29 06:45:40 +0200

    Privacy Policy

---
+++ src/pages/PrivacyPolicy.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+    <h1>Politique de confidentialité de "Francoisd.fr"</h1>
+    <p>Date de dernière mise à jour : 29/05/2023</p>
+
+    <h2>Introduction</h2>
+    <p>"Francoisd.fr" respecte votre vie privée. Nous avons rédigé cette politique de confidentialité pour vous expliquer que nous ne collectons aucune information vous concernant lorsque vous utilisez notre application.</p>
+
+    <h2>Collecte et utilisation des informations</h2>
+    <p>"Francoisd.fr" ne recueille, ne stocke ni n'utilise aucune information personnelle lorsque vous utilisez notre application. Nous ne demandons pas d'informations personnelles et nous n'avons pas la capacité de recueillir de telles informations.</p>
+
+    <h2>Sécurité des données</h2>
+    <p>Comme nous ne recueillons aucune donnée personnelle, il n'y a aucune donnée à protéger. Cependant, nous nous engageons à assurer la sécurité et l'intégrité de notre application.</p>
+
+    <h2>Modifications de la politique de confidentialité</h2>
+    <p>Nous pouvons modifier cette politique de confidentialité de temps à autre. Si nous apportons des modifications, nous vous en informerons en mettant à jour la date de dernière mise à jour de cette politique.</p>
+
+    <h2>Contact</h2>
+    <p>Si vous avez des questions sur cette politique de confidentialité, veuillez nous contacter à contact@francoisd.fr.</p>
+  </Layout>
+</template>
+
+<script>
+import Layout from '~/layouts/Default.vue'
+
+export default {
+  metaInfo: {
+    title: 'Politique de confidentialité'
+  },
+  components: {
+    Layout
+  }
+}
+</script>
